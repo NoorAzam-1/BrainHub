ed91fe1f2a35880d374a402b5f0e536bcd7f69bd
NoorAzam-1 <azamnoor698@gmail.com>
2026-05-21 17:56:18 +0530

    feat: create HomePage component with Hero, TrendingGenres, and NewArrivals

---
+++ frontend/src/app/page.js
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import TrendingGenres from "@/components/TrendingGenres";
+import NewArrivals from "@/components/NewArrivals";
+
+export default function HomePage() {
+  return ( 
+      <>
+        <Hero />
+        <TrendingGenres />
+        <NewArrivals />
+      </>
+  );
+}
