2cdb5d575ad4d19b1709b26b80fb8836d4f47556
NoorAzam-1 <azamnoor698@gmail.com>
2026-05-21 17:56:02 +0530

    feat: add layout and global styles for the application

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,41 @@
+import Header from "@/components/Header";
+import "./globals.css";
+import { Inter, Manrope } from "next/font/google";
+import Footer from "@/components/Footer";
+import { Providers } from "./provider";
+import AuthGuard from "@/components/AuthGuard";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: "--font-manrope",
+});
+
+export const metadata = {
+  title: "e-book-store",
+  description: "Modern e-book store UI",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${inter.variable} ${manrope.variable} bg-background text-on-surface`}
+      >
+        <Providers>
+          <AuthGuard>
+            <Header />
+            <main className="mt-18 sm:mt-19 md:mt-20 lg:mt-24 max-w-7xl mx-auto px-4 sm:px-6 lg:px-10">
+              {children}
+            </main>
+            <Footer />
+          </AuthGuard>
+        </Providers>
+      </body>
+    </html>
+  );
+}
